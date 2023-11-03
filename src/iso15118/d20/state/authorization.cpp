10220b4a9b0892a1984b34ddc5511aa4c5a899b6
Sebastian Lukas <sebastian.lukas@pionix.de>
2023-11-03 09:13:25 +0100

    Temporary folder restructuring of the authorization state
    Signed-off-by: Sebastian Lukas <sebastian.lukas@pionix.de>

--- src/iso15118/d20/state/authorization.cpp
+++ src/iso15118/d20/state/authorization.cpp
@@ -1,10 +1,14 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright 2023 Pionix GmbH and Contributors to EVerest
+#include <algorithm>
+
+#include <iso15118/d20/state/authorization.hpp>
+
+#include <iso15118/d20/state/service_discovery.hpp>
+
 #include <iso15118/detail/d20/state/authorization.hpp>
 
 #include <iso15118/detail/d20/context_helper.hpp>
-
-#include <algorithm>
 
 namespace iso15118::d20::state {
 
@@ -62,6 +66,35 @@ message_20::AuthorizationResponse handle_request(const message_20::Authorization
     }
 
     return response_with_code(res, response_code);
+}
+
+void Authorization::enter() {
+    ctx.log.enter_state("Authorization");
+}
+
+FsmSimpleState::HandleEventReturnType Authorization::handle_event(AllocatorType& sa, FsmEvent ev) {
+    if (ev != FsmEvent::V2GTP_MESSAGE) {
+        return sa.PASS_ON;
+    }
+
+    auto variant = ctx.get_request();
+
+    if (variant->get_type() == message_20::Type::AuthorizationReq) {
+        const auto& req = variant->get<message_20::AuthorizationRequest>();
+
+        const auto res = handle_request(req, ctx.session, ctx.config);
+
+        ctx.respond(res);
+
+        if (ctx.config.authorization_status == d20::Config::AuthStatus::Accepted) {
+            return sa.create_simple<ServiceDiscovery>(ctx);
+        } else {
+            return sa.HANDLED_INTERNALLY;
+        }
+    } else {
+        ctx.log("expected AuthorizationReq! But code type id: %d", variant->get_type());
+        return sa.PASS_ON;
+    }
 }
 
 } // namespace iso15118::d20::state
