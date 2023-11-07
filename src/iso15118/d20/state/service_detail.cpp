6bca6a2f00408f85dc038511db90e1bd0ebecab6
Sebastian Lukas <sebastian.lukas@pionix.de>
2023-11-07 10:26:34 +0100

    Refactored service_detail state folder
    Signed-off-by: Sebastian Lukas <sebastian.lukas@pionix.de>

--- src/iso15118/d20/state/service_detail.cpp
+++ src/iso15118/d20/state/service_detail.cpp
@@ -1,8 +1,12 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright 2023 Pionix GmbH and Contributors to EVerest
-#include <iso15118/detail/d20/state/service_detail.hpp>
+#include <iso15118/d20/state/service_detail.hpp>
+#include <iso15118/d20/state/service_selection.hpp>
 
 #include <iso15118/detail/d20/context_helper.hpp>
+#include <iso15118/detail/d20/state/service_detail.hpp>
+
+#include <iso15118/detail/helper.hpp>
 
 namespace iso15118::d20::state {
 
@@ -58,6 +62,32 @@ message_20::ServiceDetailResponse handle_request(const message_20::ServiceDetail
     }
 
     return response_with_code(res, message_20::ResponseCode::OK);
+}
+
+void ServiceDetail::enter() {
+    ctx.log.enter_state("ServiceDetail");
+}
+
+FsmSimpleState::HandleEventReturnType ServiceDetail::handle_event(AllocatorType& sa, FsmEvent ev) {
+
+    if (ev != FsmEvent::V2GTP_MESSAGE) {
+        return sa.PASS_ON;
+    }
+
+    const auto variant = ctx.get_request();
+
+    if (const auto req = variant->get_if<message_20::ServiceDetailRequest>()) {
+        logf("Requested info about ServiceID: %d\n", req->service);
+
+        const auto res = handle_request(*req, ctx.session, ctx.config);
+
+        ctx.respond(res);
+
+        return sa.create_simple<ServiceSelection>(ctx);
+    } else {
+        ctx.log("expected ServiceDetailReq! But code type id: %d", variant->get_type());
+        return sa.PASS_ON;
+    }
 }
 
 } // namespace iso15118::d20::state
