a386068c5aeff447433c4a2cbfc0038150225f63
Sebastian Lukas <sebastian.lukas@pionix.de>
2023-11-07 11:10:27 +0100

    Refactored schedule_exchange state folder
    Signed-off-by: Sebastian Lukas <sebastian.lukas@pionix.de>

---
+++ src/iso15118/d20/state/schedule_exchange.cpp
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2023 Pionix GmbH and Contributors to EVerest
+#include <iso15118/d20/state/dc_cable_check.hpp>
+#include <iso15118/d20/state/schedule_exchange.hpp>
+
+#include <iso15118/detail/d20/context_helper.hpp>
+#include <iso15118/detail/d20/state/schedule_exchange.hpp>
+
+namespace iso15118::d20::state {
+
+message_20::ScheduleExchangeResponse handle_request(const message_20::ScheduleExchangeRequest& req,
+                                                    const d20::Session& session) {
+
+    message_20::ScheduleExchangeResponse res;
+
+    if (validate_and_setup_header(res.header, session, req.header.session_id) == false) {
+        return response_with_code(res, message_20::ResponseCode::FAILED_UnknownSession);
+    }
+
+    res.processing = message_20::Processing::Finished;
+
+    return response_with_code(res, message_20::ResponseCode::OK);
+}
+
+void ScheduleExchange::enter() {
+    ctx.log.enter_state("ScheduleExchange");
+}
+
+FsmSimpleState::HandleEventReturnType ScheduleExchange::handle_event(AllocatorType& sa, FsmEvent ev) {
+    if (ev != FsmEvent::V2GTP_MESSAGE) {
+        return sa.PASS_ON;
+    }
+
+    const auto variant = ctx.get_request();
+
+    if (const auto req = variant->get_if<message_20::ScheduleExchangeRequest>()) {
+        const auto res = handle_request(*req, ctx.session);
+
+        ctx.respond(res);
+
+        return sa.create_simple<DC_CableCheck>(ctx);
+    } else {
+        ctx.log("expected ScheduleExchangeReq! But code type id: %d", variant->get_type());
+        return sa.PASS_ON;
+    }
+}
+
+} // namespace iso15118::d20::state
