d7a943a206233c287a2c6f8c2699718cc2761b10
Sebastian Lukas <sebastian.lukas@pionix.de>
2023-11-07 10:11:40 +0100

    Refactored authorization_setup stated folder
    Signed-off-by: Sebastian Lukas <sebastian.lukas@pionix.de>

--- src/iso15118/d20/state/authorization_setup.cpp
+++ src/iso15118/d20/state/authorization_setup.cpp
@@ -1,11 +1,14 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright 2023 Pionix GmbH and Contributors to EVerest
-#include <iso15118/detail/d20/state/authorization_setup.hpp>
+#include <random>
+
+#include <iso15118/d20/state/authorization.hpp>
+#include <iso15118/d20/state/authorization_setup.hpp>
 
 #include <iso15118/detail/d20/context_helper.hpp>
 #include <iso15118/detail/helper.hpp>
 
-#include <random>
+#include <iso15118/detail/d20/state/authorization_setup.hpp>
 
 namespace iso15118::d20::state {
 
@@ -50,6 +53,32 @@ message_20::AuthorizationSetupResponse handle_request(const message_20::Authoriz
     }
 
     return response_with_code(res, message_20::ResponseCode::OK);
+}
+
+void AuthorizationSetup::enter() {
+    ctx.log.enter_state("AuthorizationSetup");
+}
+
+FsmSimpleState::HandleEventReturnType AuthorizationSetup::handle_event(AllocatorType& sa, FsmEvent ev) {
+
+    if (ev != FsmEvent::V2GTP_MESSAGE) {
+        return sa.PASS_ON;
+    }
+
+    const auto variant = ctx.get_request();
+
+    if (const auto req = variant->get_if<message_20::AuthorizationSetupRequest>()) {
+        const auto res = handle_request(*req, ctx.session, ctx.config);
+
+        logf("Timestamp: %d\n", req->header.timestamp);
+
+        ctx.respond(res);
+
+        return sa.create_simple<Authorization>(ctx);
+    } else {
+        ctx.log("expected AuthorizationSetupReq! But code type id: %d", variant->get_type());
+        return sa.PASS_ON;
+    }
 }
 
 } // namespace iso15118::d20::state
