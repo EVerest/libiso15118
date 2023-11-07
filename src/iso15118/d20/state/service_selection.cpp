24b1574559165f400013ee8b1cf892146fb0ea3b
Sebastian Lukas <sebastian.lukas@pionix.de>
2023-11-07 10:35:32 +0100

    Refactored service_selection state folder
    Signed-off-by: Sebastian Lukas <sebastian.lukas@pionix.de>

--- src/iso15118/d20/state/service_selection.cpp
+++ src/iso15118/d20/state/service_selection.cpp
@@ -1,8 +1,12 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright 2023 Pionix GmbH and Contributors to EVerest
-#include <iso15118/detail/d20/state/service_selection.hpp>
+#include <iso15118/d20/state/dc_charge_parameter_discovery.hpp>
+#include <iso15118/d20/state/service_selection.hpp>
 
 #include <iso15118/detail/d20/context_helper.hpp>
+#include <iso15118/detail/d20/state/service_detail.hpp>
+#include <iso15118/detail/d20/state/service_selection.hpp>
+#include <iso15118/detail/helper.hpp>
 
 namespace iso15118::d20::state {
 
@@ -64,6 +68,38 @@ message_20::ServiceSelectionResponse handle_request(const message_20::ServiceSel
     // TODO(sl): Really important: Save the vas selected services in session!!!
 
     return response_with_code(res, message_20::ResponseCode::OK);
+}
+
+void ServiceSelection::enter() {
+    ctx.log.enter_state("ServiceSelection");
+}
+
+FsmSimpleState::HandleEventReturnType ServiceSelection::handle_event(AllocatorType& sa, FsmEvent ev) {
+
+    if (ev != FsmEvent::V2GTP_MESSAGE) {
+        return sa.PASS_ON;
+    }
+
+    const auto variant = ctx.get_request();
+
+    if (const auto req = variant->get_if<message_20::ServiceDetailRequest>()) {
+        logf("Requested info about ServiceID: %d\n", req->service);
+
+        const auto res = handle_request(*req, ctx.session, ctx.config);
+
+        ctx.respond(res);
+
+        return sa.HANDLED_INTERNALLY;
+    } else if (const auto req = variant->get_if<message_20::ServiceSelectionRequest>()) {
+        const auto res = handle_request(*req, ctx.session, ctx.config);
+
+        ctx.respond(res);
+
+        return sa.create_simple<DC_ChargeParameterDiscovery>(ctx);
+    } else {
+        ctx.log("expected ServiceDetailReq! But code type id: %d", variant->get_type());
+        return sa.PASS_ON;
+    }
 }
 
 } // namespace iso15118::d20::state
