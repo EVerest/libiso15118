ba47d8da5fd8e5a19d234c77402ab7a5f3071fd5
Sebastian Lukas <sebastian.lukas@pionix.de>
2023-12-08 14:03:00 +0100

    Refactored session_setup state
    Signed-off-by: Sebastian Lukas <sebastian.lukas@pionix.de>

--- src/iso15118/d20/state/session_setup.cpp
+++ src/iso15118/d20/state/session_setup.cpp
@@ -1,15 +1,13 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright 2023 Pionix GmbH and Contributors to EVerest
+#include <algorithm>
+
+#include <iso15118/d20/state/authorization_setup.hpp>
 #include <iso15118/d20/state/session_setup.hpp>
 
 #include <iso15118/detail/d20/context_helper.hpp>
+#include <iso15118/detail/d20/state/session_setup.hpp>
 #include <iso15118/detail/helper.hpp>
-
-#include <iso15118/d20/state/authorization_setup.hpp>
-
-#include <iso15118/message/session_setup.hpp>
-
-#include <algorithm>
 
 static bool session_is_zero(const iso15118::message_20::Header& header) {
     return std::all_of(header.session_id.begin(), header.session_id.end(), [](int i) { return i == 0; });
@@ -17,46 +15,68 @@ static bool session_is_zero(const iso15118::message_20::Header& header) {
 
 namespace iso15118::d20::state {
 
+message_20::SessionSetupResponse handle_request(const message_20::SessionSetupRequest& req, const d20::Session& session,
+                                                const std::string evse_id, bool new_session) {
+
+    message_20::SessionSetupResponse res;
+    setup_header(res.header, session);
+
+    res.evseid = evse_id;
+
+    if (new_session) {
+        return response_with_code(res, message_20::ResponseCode::OK_NewSessionEstablished);
+    } else {
+        return response_with_code(res, message_20::ResponseCode::OK_OldSessionJoined);
+    }
+}
+
 void SessionSetup::enter() {
     ctx.log.enter_state("SessionSetup");
 }
 
 FsmSimpleState::HandleEventReturnType SessionSetup::handle_event(AllocatorType& sa, FsmEvent ev) {
-    if (ev == FsmEvent::V2GTP_MESSAGE) {
-        auto variant = ctx.get_request();
-        if (variant->get_type() != message_20::Type::SessionSetupReq) {
-            ctx.log("expected SessionSetupReq! But code type id: %d", variant->get_type());
-            return sa.PASS_ON;
-        }
 
-        const auto& req = variant->get<message_20::SessionSetupRequest>();
-
-        logf("Received session setup with evccid: %s\n", req.evccid.c_str());
-
-        message_20::SessionSetupResponse response{};
-
-        ctx.config = Config(); // todo(sl): config is initialized here temporarily, needs refactoring
-
-        if (session_is_zero(req.header)) {
-            ctx.session = Session();
-            response.response_code = message_20::ResponseCode::OK_NewSessionEstablished;
-        } else if (req.header.session_id == ctx.session.get_id()) {
-            response.response_code = message_20::ResponseCode::OK_OldSessionJoined;
-        } else {
-            ctx.session = Session();
-            response.response_code = message_20::ResponseCode::OK_NewSessionEstablished;
-        }
-
-        setup_header(response.header, ctx.session);
-
-        response.evseid = "everest se";
-
-        ctx.respond(response);
-
-        return sa.create_simple<AuthorizationSetup>(ctx);
+    if (ev != FsmEvent::V2GTP_MESSAGE) {
+        return sa.PASS_ON;
     }
 
-    return sa.PASS_ON;
+    const auto variant = ctx.get_request();
+
+    if (const auto req = variant->get_if<message_20::SessionSetupRequest>()) {
+
+        logf("Received session setup with evccid: %s\n", req->evccid.c_str());
+        ctx.config = Config(); // todo(sl): config is initialized here temporarily, needs refactoring
+
+        bool new_session{true};
+
+        if (session_is_zero(req->header)) {
+            ctx.session = Session();
+        } else if (req->header.session_id == ctx.session.get_id()) {
+            new_session = false;
+        } else {
+            ctx.session = Session();
+        }
+
+        evse_id = "everest se"; // Todo(sl): Use correct evse id from setup control_message
+
+        const auto res = handle_request(*req, ctx.session, evse_id, new_session);
+
+        ctx.respond(res);
+
+        return sa.create_simple<AuthorizationSetup>(ctx);
+
+        // Todo(sl): Going straight to ChargeParameterDiscovery?
+
+    } else {
+        ctx.log("expected SessionSetupReq! But code type id: %d", variant->get_type());
+
+        // Sequence Error
+        const message_20::Type req_type = variant->get_type();
+        send_sequence_error(req_type, ctx);
+
+        ctx.session_stopped = true;
+        return sa.PASS_ON;
+    }
 }
 
 } // namespace iso15118::d20::state
