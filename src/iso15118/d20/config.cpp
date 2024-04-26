42a6ead9298ce9054e1b6b091118d859148e8b29
Sebastian Lukas <sebastian.lukas@pionix.de>
2024-04-26 13:30:52 +0200

    Better struct initialization for ac_parameter_list
    Signed-off-by: Sebastian Lukas <sebastian.lukas@pionix.de>

--- src/iso15118/d20/config.cpp
+++ src/iso15118/d20/config.cpp
@@ -10,7 +10,7 @@ SessionConfig::SessionConfig() {
     supported_energy_transfer_services = {message_20::ServiceCategory::DC, message_20::ServiceCategory::DC_BPT};
 
     ac_parameter_list.push_back({
-        message_20::AcConnector::ThreePhase,
+        message_20::AcConnector::SinglePhase,
         message_20::ControlMode::Scheduled,
         message_20::MobilityNeedsMode::ProvidedByEvcc,
         16,
@@ -19,7 +19,7 @@ SessionConfig::SessionConfig() {
 
     ac_bpt_parameter_list.push_back({
         {
-            message_20::AcConnector::ThreePhase,
+            message_20::AcConnector::SinglePhase,
             message_20::ControlMode::Scheduled,
             message_20::MobilityNeedsMode::ProvidedByEvcc,
             16,
@@ -46,6 +46,34 @@ SessionConfig::SessionConfig() {
                                      message_20::BptChannel::Unified,
                                      message_20::GeneratorMode::GridFollowing});
 
+    evse_ac_parameter = {
+        {11, 3},                          // max_charge_power
+        message_20::RationalNumber{0, 0}, // max_charge_power_L2
+        message_20::RationalNumber{0, 0}, // max_charge_power_L3
+        {23, 2},                          // min_charge_power
+        message_20::RationalNumber{0, 0}, // min_charge_power_L2
+        message_20::RationalNumber{0, 0}, // min_charge_power_L3
+        {50, 0},                          // nominal_frequency
+    };
+
+    evse_ac_bpt_parameter = {
+        {
+            {11, 3},                          // max_charge_power
+            message_20::RationalNumber{0, 0}, // max_charge_power_L2
+            message_20::RationalNumber{0, 0}, // max_charge_power_L3
+            {23, 2},                          // min_charge_power
+            message_20::RationalNumber{0, 0}, // min_charge_power_L2
+            message_20::RationalNumber{0, 0}, // min_charge_power_L3
+            {50, 0},                          // nominal_frequency
+        },
+        {11, 3},                           // max_discharge_power
+        message_20::RationalNumber{0, 0}, // max_discharge_power_L2
+        message_20::RationalNumber{0, 0}, // max_discharge_power_L3
+        {23, 2},                            // min_charge_power
+        message_20::RationalNumber{0, 0},  // min_discharge_power_L2
+        message_20::RationalNumber{0, 0},  // min_discharge_power_L3
+    };
+
     evse_dc_parameter = {
         {22, 3},  // max_charge_power
         {0, 0},   // min_charge_power
@@ -69,16 +97,6 @@ SessionConfig::SessionConfig() {
         {25, 0}, // max_discharge_current
         {0, 0},  // min_discharge_current
     };
-
-    evse_ac_parameter.max_charge_power = {11, 3};
-    evse_ac_parameter.min_charge_power = {11, 3};
-    evse_ac_parameter.nominal_frequency = {50, 0};
-
-    evse_ac_bpt_parameter.max_charge_power = {11, 3};
-    evse_ac_bpt_parameter.min_charge_power = {23, 2};
-    evse_ac_bpt_parameter.nominal_frequency = {50, 0};
-    evse_ac_bpt_parameter.max_discharge_power = {11, 3};
-    evse_ac_bpt_parameter.min_discharge_power = {23, 2};
 }
 
 } // namespace iso15118::d20
