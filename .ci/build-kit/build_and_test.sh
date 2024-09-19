258f9481d8917eb1d1429d4f4493e5544747e4e8
Sebastian Lukas <sebastian.lukas@pionix.de>
2024-09-19 12:15:57 +0200

    Adding suggestions, enabled default compiler warnings as errors
    Signed-off-by: Sebastian Lukas <sebastian.lukas@pionix.de>

--- .ci/build-kit/build_and_test.sh
+++ .ci/build-kit/build_and_test.sh
@@ -7,8 +7,7 @@ cmake \
     -S "$EXT_MOUNT/source" \
     -G Ninja \
     -DBUILD_TESTING=ON  \
-    -DCMAKE_EXPORT_COMPILE_COMMANDS=ON  \
-    -DISO15118_ENABLE_COMPILE_WARNINGS=ON
+    -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 
 ninja -j$(nproc) -C build
 
