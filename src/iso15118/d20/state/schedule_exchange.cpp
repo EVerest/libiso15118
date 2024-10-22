e75f4dc694e29f9cce5c07c524826c0b2f18f2d4
Sebastian Lukas <sebastian.lukas@pionix.de>
2024-10-22 15:01:44 +0200

    Adding helper function and added aliases for better readability
    Signed-off-by: Sebastian Lukas <sebastian.lukas@pionix.de>

--- src/iso15118/d20/state/schedule_exchange.cpp
+++ src/iso15118/d20/state/schedule_exchange.cpp
@@ -12,6 +12,33 @@
 
 namespace iso15118::d20::state {
 
+using ScheduledReqControlMode = message_20::ScheduleExchangeRequest::Scheduled_SEReqControlMode;
+using ScheduledResControlMode = message_20::ScheduleExchangeResponse::Scheduled_SEResControlMode;
+
+using DynamicReqControlMode = message_20::ScheduleExchangeRequest::Dynamic_SEReqControlMode;
+using DynamicResControlMode = message_20::ScheduleExchangeResponse::Dynamic_SEResControlMode;
+
+namespace {
+auto create_default_scheduled_control_mode(const message_20::RationalNumber& max_power) {
+    message_20::ScheduleExchangeResponse::ScheduleTuple schedule;
+    schedule.schedule_tuple_id = 1;
+    schedule.charging_schedule.power_schedule.time_anchor =
+        static_cast<uint64_t>(std::time(nullptr)); // PowerSchedule is now active
+
+    message_20::ScheduleExchangeResponse::PowerScheduleEntry power_schedule;
+    power_schedule.power = max_power;
+    power_schedule.duration = message_20::ScheduleExchangeResponse::SCHEDULED_POWER_DURATION_S;
+    schedule.charging_schedule.power_schedule.entries.push_back(power_schedule);
+
+    ScheduledResControlMode scheduled_mode{};
+
+    // Providing no price schedule!
+    // NOTE: Agreement on iso15118.elaad.io: [V2G20-2176] is not required and should be ignored.
+    scheduled_mode.schedule_tuple = {schedule};
+    return scheduled_mode;
+}
+} // namespace
+
 message_20::ScheduleExchangeResponse
 handle_request(const message_20::ScheduleExchangeRequest& req, const d20::Session& session,
                const message_20::RationalNumber& max_power, std::optional<std::time_t> new_departure_time,
@@ -26,33 +53,18 @@ handle_request(const message_20::ScheduleExchangeRequest& req, const d20::Sessio
     // Todo(SL): Publish data from request?
 
     if (session.get_selected_control_mode() == message_20::ControlMode::Scheduled &&
-        std::holds_alternative<message_20::ScheduleExchangeRequest::Scheduled_SEReqControlMode>(req.control_mode)) {
-        auto& control_mode =
-            res.control_mode.emplace<message_20::ScheduleExchangeResponse::Scheduled_SEResControlMode>();
+        std::holds_alternative<ScheduledReqControlMode>(req.control_mode)) {
 
-        message_20::ScheduleExchangeResponse::ScheduleTuple schedule;
-        schedule.schedule_tuple_id = 1;
-        schedule.charging_schedule.power_schedule.time_anchor =
-            static_cast<uint64_t>(std::time(nullptr)); // PowerSchedule is now active
-
-        message_20::ScheduleExchangeResponse::PowerScheduleEntry power_schedule;
-        power_schedule.power = max_power;
-        power_schedule.duration = message_20::ScheduleExchangeResponse::SCHEDULED_POWER_DURATION_S;
-        schedule.charging_schedule.power_schedule.entries.push_back(power_schedule);
-
-        // Providing no price schedule!
-        // NOTE: Agreement on iso15118.elaad.io: [V2G20-2176] is not required and should be ignored.
-        control_mode.schedule_tuple.push_back(schedule);
+        res.control_mode.emplace<ScheduledResControlMode>(create_default_scheduled_control_mode(max_power));
 
         // TODO(sl): Adding price schedule
         // TODO(sl): Adding discharging schedule
 
     } else if (session.get_selected_control_mode() == message_20::ControlMode::Dynamic &&
-               std::holds_alternative<message_20::ScheduleExchangeRequest::Dynamic_SEReqControlMode>(
-                   req.control_mode)) {
+               std::holds_alternative<DynamicReqControlMode>(req.control_mode)) {
 
         // TODO(sl): Publish req dynamic mode parameters
-        auto& mode = res.control_mode.emplace<message_20::ScheduleExchangeResponse::Dynamic_SEResControlMode>();
+        auto& mode = res.control_mode.emplace<DynamicResControlMode>();
 
         if (session.get_selected_mobility_needs_mode() == message_20::MobilityNeedsMode::ProvidedBySecc) {
             if (new_departure_time.has_value()) {
