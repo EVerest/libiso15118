509f444b30f6333506ff49a3f7d1f74f4a401a0a
Sebastian Lukas <sebastian.lukas@pionix.de>
2024-11-19 12:25:06 +0100

    Fixing clang compiler warnings
    Signed-off-by: Sebastian Lukas <sebastian.lukas@pionix.de>

--- src/iso15118/tbd_controller.cpp
+++ src/iso15118/tbd_controller.cpp
@@ -28,8 +28,8 @@ void TbdController::loop() {
 
     if (not config.enable_sdp_server) {
         auto connection = std::make_unique<io::ConnectionPlain>(poll_manager, config.interface_name);
-        session =
-            std::make_unique<Session>(std::move(connection), std::move(d20::SessionConfig(evse_setup)), callbacks);
+        auto session_config = d20::SessionConfig(evse_setup);
+        session = std::make_unique<Session>(std::move(connection), std::move(session_config), callbacks);
     }
 
     auto next_event = get_current_time_point();
@@ -48,8 +48,8 @@ void TbdController::loop() {
 
                 if (not config.enable_sdp_server) {
                     auto connection = std::make_unique<io::ConnectionPlain>(poll_manager, config.interface_name);
-                    session = std::make_unique<Session>(std::move(connection),
-                                                        std::move(d20::SessionConfig(evse_setup)), callbacks);
+                    auto session_config = d20::SessionConfig(evse_setup);
+                    session = std::make_unique<Session>(std::move(connection), std::move(session_config), callbacks);
                 }
             }
         }
@@ -112,7 +112,8 @@ void TbdController::handle_sdp_server_input() {
 
     const auto ipv6_endpoint = connection->get_public_endpoint();
 
-    session = std::make_unique<Session>(std::move(connection), std::move(d20::SessionConfig(evse_setup)), callbacks);
+    auto session_config = d20::SessionConfig(evse_setup);
+    session = std::make_unique<Session>(std::move(connection), std::move(session_config), callbacks);
 
     sdp_server->send_response(request, ipv6_endpoint);
 }
