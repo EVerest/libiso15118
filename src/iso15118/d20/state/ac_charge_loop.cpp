7331a472f742e0478a64f36f2401edaced89d3e0
AssemblyJohn <ioan.bogdann@gmail.com>
2025-02-11 17:21:28 +0200

    Updated copyright
    Signed-off-by: AssemblyJohn <ioan.bogdann@gmail.com>

--- src/iso15118/d20/state/ac_charge_loop.cpp
+++ src/iso15118/d20/state/ac_charge_loop.cpp
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2025 Pionix GmbH and Contributors to EVerest
 #include <iso15118/d20/state/ac_charge_loop.hpp>
 
 namespace iso15118::d20::state {
